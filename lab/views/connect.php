e043f2f90a151b318ab21cfcf57416e093c63487
SANTISOFT <santimasoft@gmail.com>
2024-03-11 18:54:38 +0200

    test

--- lab/views/connect.php
+++ lab/views/connect.php
@@ -1,5 +1,8 @@
 <?php
+$servername = "localhost";
+$username = "u106709385_root";
+$password = "Santima@123";
 
-$con = mysqli_connect("localhost", "u106709385_root", "Santima@123", "u106709385_lms20");
+$con = mysqli_connect("$servername", "$username", "$password", "u106709385_lms20");
 
 ?>
